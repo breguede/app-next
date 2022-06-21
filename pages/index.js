e7a60b53ba7bd110db9160786dd145f946a6094a
Danilo Andrade <danilo@casadosaber.com.br>
2022-06-20 21:21:30 -0300

    Adicionando variaveis de ambiente

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,5 @@
 import { useState } from 'react';
 import  { analista } from './api/analista'
 
-function Home(){
-    return (
-        <div>
-            <h1>HOME</h1>
-            <Contador />
-            <div>teste</div>
-        </div>
-    )
-}
-
-function Contador(){
-    let listaAnalista = analista
-    return (
-        <div>
-            <div>{listaAnalista}</div>
-            
-        </div>
-    )    
-}
 
 export default Home;
